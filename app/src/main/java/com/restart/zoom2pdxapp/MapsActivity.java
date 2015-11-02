451bd4037c42f0e59c546fa7bd2e9fd9795fae33
restart32 <kiarashbikar@gmail.com>
2015-11-01 22:48:48 -0800

    A nested loop is now implemented
     where after dragging near by schools, it gets their school id and will request more information from each
     school using their school id and will parse another set of JSON objects. Removed the scoring system for now so everything gets displays as 0.0 BAD.

--- app/src/main/java/com/restart/zoom2pdxapp/MapsActivity.java
+++ app/src/main/java/com/restart/zoom2pdxapp/MapsActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +23,7 @@ import org.json.JSONObject;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
@@ -36,7 +38,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -47,10 +48,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -73,7 +70,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (total == 1) {
             circleOptions = new CircleOptions()
                     .center(latLng)
-                    .radius(2000)
+                    .radius(1500)
                     .strokeColor(Color.BLUE)
                     .fillColor(0x40ff0000)
                     .strokeWidth(5);
@@ -94,7 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else if (total == 2) {
             circleOptions = new CircleOptions()
                     .center(latLng)
-                    .radius(2000)
+                    .radius(1500)
                     .strokeColor(Color.BLUE)
                     .fillColor(R.color.red_global_color)
                     .strokeWidth(5);
@@ -121,6 +118,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AsyncTask.execute(new Runnable() {
             public void run() {
                 String strContent = "";
+                String strContent2 = "";
 
                 try {
                     URL urlHandle = new URL("http://api.civicapps.org/schools/near/" + latLngParameter.longitude + "," + latLngParameter.latitude);
@@ -159,20 +157,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     for (int i = 0; i < results.length(); i += 1) {
                         JSONObject result = results.getJSONObject(i);
 
+                        final String school_id = String.valueOf(result.getString("SchoolID"));
                         final String school_name = String.valueOf(result.getString("SchoolName"));
                         final String phone_number = String.valueOf(result.getString("PhoneNumber"));
                         final double latParameter = Double.parseDouble(result.getString("Latitude"));
                         final double lngParameter = Double.parseDouble(result.getString("Longitude"));
-                        circlevalue = (Double.parseDouble(result.getString("distance")) * 4);
-                        result = results.getJSONObject(++i);
-                        circlevalue2 = (Double.parseDouble(result.getString("distance")) * 4);
-                        --i;
+
                         MapsActivity.this.runOnUiThread(new Runnable() {
                             public void run() {
                                 mMap.addMarker(new MarkerOptions().position(new LatLng(latParameter, lngParameter))
                                         .title(school_name).snippet(phone_number));
                             }
                         });
+
+                        try {
+                            URL urlHandle = new URL("http://api.civicapps.org/schools/" + school_id);
+                            URLConnection urlconnectionHandle = urlHandle.openConnection();
+                            InputStream inputstreamHandle = urlconnectionHandle.getInputStream();
+
+                            try {
+                                int intRead;
+                                byte[] byteBuffer = new byte[1024];
+
+                                do {
+                                    intRead = inputstreamHandle.read(byteBuffer);
+
+                                    if (intRead == 0) {
+                                        break;
+
+                                    } else if (intRead == -1) {
+                                        break;
+
+                                    }
+
+                                    strContent2 += new String(byteBuffer, 0, intRead, "UTF-8");
+                                } while (true);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            inputstreamHandle.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(strContent2).getJSONObject("results").getJSONObject("SchoolInformation").getJSONObject("Dropouts");
+
+                            final double dropout_previous = Double.parseDouble(String.valueOf(jsonObject.get("StateDropoutStudentsPctPrevYear")));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
